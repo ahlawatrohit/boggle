ee7efa864cad5c5674f7428a7fdbbd641fc72174
Rohit Ahlawat <rohit@Rohits-MacBook-Air.local>
2020-03-28 23:46:42 -0400

    add error handler and requirement txt file

--- game/views.py
+++ game/views.py
@@ -1,34 +1,47 @@
 from django.http import HttpResponse
 from django.template import Context, RequestContext
 from django.shortcuts import  get_object_or_404
+from django.http import Http404
+from rest_framework.decorators import api_view
 import json
 from .game_manager import GameManager
 
+@api_view(["GET"])
 def start_game(request):
     return HttpResponse(
         json.dumps(GameManager.create_new_game()),
         content_type='application/json'
     )
 
+@api_view(["GET"])
 def check_word(request):
-    word = request.GET['word']
-    game_id = request.GET['game_id']
-    print (word)
-    print (game_id)
+    try:
+        word = request.GET['word']
+        game_id = request.GET['game_id']
+    except Exception as e:
+        raise Http404("Game id and word are required fields")
     return HttpResponse(
         json.dumps(GameManager.check_word(game_id, word.lower())),
         content_type='application/json'
     )
 
+@api_view(["GET"])
 def get_game_words(request):
-    game_id = request.GET['game_id']
+    try:
+        game_id = request.GET['game_id']
+    except Exception as e:
+        raise Http404("Game id is a required field")
     return HttpResponse(
         json.dumps(GameManager.get_game_words(game_id)),
         content_type='application/json'
     )
 
+@api_view(["GET"])
 def rotate_game_board(request):
-    game_id = request.GET['game_id']
+    try:
+        game_id = request.GET['game_id']
+    except Exception as e:
+        raise Http404("Game id is a required field")
     return HttpResponse(
         json.dumps(GameManager.rotate_game_grid(game_id)),
         content_type='application/json'
